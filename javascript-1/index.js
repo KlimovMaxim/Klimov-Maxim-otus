237dd864d562d5f41bb2bb4c2dabe25d2a25e240
Климов Максим Леонидович <17022983@sigma.sbrf.ru>
2019-07-02 11:44:50 +0300

    template

---
+++ javascript-1/index.js
@@ -0,0 +1,15 @@
+function sum(a) {
+
+  var currentSum = a;
+
+  function f(b) {
+    currentSum += b;
+    return f;
+  }
+
+  f.toString = function() {
+    return currentSum;
+  };
+
+  return f;
+}
