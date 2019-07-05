d0193ebf03e048d6ada26b4bb752dc568822db88
Maxim Klimov <Klimov.m.l@yandex.ru>
2019-07-05 19:20:30 +0300

    ready

--- javascript-1/index.js
+++ javascript-1/index.js
@@ -1,16 +1,17 @@
 function sum(a) {
-
   let currentSum = a;
 
   function f(b) {
-    if (b !== null){
-    currentSum += b;}
+    if (b == undefined) {
+      currentSum = currentSum;
+    }
+    else { currentSum += b; }
     return f;
   }
 
-  f.toString = function() {
+  f.toString = function () {
     return currentSum;
   };
-  
+
   return f;
 }
