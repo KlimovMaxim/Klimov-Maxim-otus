c0c8776d49d714c30669ac78c163541f52f05493
Климов Максим Леонидович <17022983@sigma.sbrf.ru>
2019-07-08 16:07:29 +0300

    JS2 init

---
+++ javascript-2/index.js
@@ -0,0 +1,37 @@
+
+var fn1 = () => {
+console.log('fn1')
+return Promise.resolve(1)
+}
+
+var fn2 = () => new Promise(resolve => {
+console.log('fn2')
+setTimeout(() => resolve(2), 1000)
+})
+
+function promiseReduce(asyncFunctions, reduce, initialValue) { 
+/* 
+* Реализация
+*/
+}
+
+promiseReduce(
+[fn1, fn2], 
+function (memo, value) {
+console.log('reduce')
+return memo * value
+}, 
+1
+)
+.then(console.log) 
+
+
+
+//Вывод в консоль
+```
+fn1
+reduce
+fn2
+reduce
+2
+```
