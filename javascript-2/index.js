76e51e9f68e612af5db347a3b3dfcbfba4e1500f
Maxim Klimov <Klimov.m.l@yandex.ru>
2019-07-11 22:08:57 +0300

    должно быть похоже на это

--- javascript-2/index.js
+++ javascript-2/index.js
@@ -1,37 +1,39 @@
-
 var fn1 = () => {
-console.log('fn1')
-return Promise.resolve(1)
+    console.log('fn1')
+    return Promise.resolve(1)
 }
 
 var fn2 = () => new Promise(resolve => {
-console.log('fn2')
-setTimeout(() => resolve(2), 1000)
+    console.log('fn2')
+    setTimeout(() => resolve(2), 1000)
 })
 
-function promiseReduce(asyncFunctions, reduce, initialValue) { 
-/* 
-* Реализация
-*/
+
+function promiseReduce(asyncFunctions, reduce, initialValue) {
+    return new Promise(function (resolve, reject) {
+        let memo = initialValue;
+
+        const reduceCalling = f => {
+            //console.log(memo,f);
+            f().then(x => memo = reduce.call(promiseReduce, memo, x));
+        };
+
+        for (f of asyncFunctions) reduceCalling(f);
+        //asyncFunctions.forEach(f => reduceCalling(f));
+
+        
+        Promise.all(asyncFunctions).then(resolve(memo));
+    })
 }
 
+
+
 promiseReduce(
-[fn1, fn2], 
-function (memo, value) {
-console.log('reduce')
-return memo * value
-}, 
-1
-)
-.then(console.log) 
-
-
-
-//Вывод в консоль
-```
-fn1
-reduce
-fn2
-reduce
-2
-```
+        [fn1, fn2],
+        function (memo, value) {
+            console.log('reduce')
+            return memo * value
+        },
+        1
+    )
+    .then(a => console.log(a));
