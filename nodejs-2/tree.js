0fdc97ab168ecbce7d035eee5db136110592d521
Maxim Klimov <Klimov.m.l@yandex.ru>
2019-08-11 13:51:20 +0300

    node 2 not async

--- nodejs-2/tree.js
+++ nodejs-2/tree.js
@@ -1,28 +1,49 @@
-const fs = require('fs')
-const fsP = fs.promises;
-const pth = require('path')
-const files = [];
-const folders = [];
+const fs = require('fs');
+const fsPromises = fs.promises;
+const p = require('path');
 
-const chdir = (p,ar) =>
-{
- for (const dirent of ar) {
-    let newPath = pth.join(p, dirent.name);
-    if (dirent.isDirectory()) {
-        folders.push(newPath)
-        console.log(folders);
-    } else if (dirent.isFile()) {
-        files.push(newPath)
-        console.log(files);
+
+class Tree {
+    constructor(dirname){
+        console.log(dirname)
+        this.dirname = dirname;
+        this.files = [];
+        this.folders = [];
     }
- }   
+    
+    async run(){
+        console.log('run start dirname='+this.dirname)
+       await this.tree(this.dirname);
+    }
+
+    async tree(ph) {
+        console.log('tree start patch='+ph)
+        const pth = ph;
+        
+        const chdir = (err,ar) =>
+        {
+            for (const dirent of ar) {
+               let newPath = p.join(pth, dirent.name);
+               if (dirent.isDirectory()) {
+                   this.folders.push(newPath);
+                   this.tree(newPath);
+                  // this.promises.push();
+                //  console.log(newPath + ' isDirectory');
+               } else if (dirent.isFile()) {
+                   this.files.push(newPath);
+               //    console.log(newPath + ' isFile');
+               }
+            }  
+        
+        }
+       
+        //return fsPromises.readdir(path,{withFileTypes:true}).then(ar => chdir(path,ar));
+        fs.readdir(path,{withFileTypes:true},chdir(err,ar))
+    }
 }
 
 
-const tree = (path) => {
-    fsP.readdir(path,{withFileTypes:true}).then(a => chdir(path,a)) 
-}
-
-tree('.');
-console.log(files);
-console.log(folders);
+const t = new Tree('.');
+console.log(t.run())
+console.log(t.files);
+console.log(t.folders);
